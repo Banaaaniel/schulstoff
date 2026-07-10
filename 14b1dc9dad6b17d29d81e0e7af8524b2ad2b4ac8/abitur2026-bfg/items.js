55586db36c1d8f1ca1a2963c8f3f6331789b7d0d
Banaaaniel <145291016+Banaaaniel@users.noreply.github.com>
2026-07-10 20:42:38 +0200

    Update items.js

--- 14b1dc9dad6b17d29d81e0e7af8524b2ad2b4ac8/abitur2026-bfg/items.js
+++ 14b1dc9dad6b17d29d81e0e7af8524b2ad2b4ac8/abitur2026-bfg/items.js
@@ -14,4 +14,39 @@ window.subjectItems = [
     //     type: 'folder',
     //     path: 'resources/index.html'
     // }
+    {
+        name: 'Erster Weltkrieg',
+        type: 'folder',
+        pathe: 'erster-weltkrieg/index.html'
+    },
+    {
+        name: 'Herrschaftsmodelle 20. Jahrhundert',
+        type: 'folder',
+        pathe: 'herrschaftsmodelle-20jh/index.html'
+    },
+    {
+        name: 'Industrialisierung',
+        type: 'folder',
+        pathe: 'industrialisierung/index.html'
+    },
+    {
+        name: 'Klausuren und Abitur',
+        type: 'folder',
+        pathe: 'klausuren-abitur/index.html'
+    },
+    {
+        name: 'Methoden und Arbeitsweisen',
+        type: 'folder',
+        pathe: 'methoden-und-arbeiten/index.html'
+    },
+    {
+        name: 'Zweiter Weltkrieg',
+        type: 'folder',
+        pathe: 'zweiter-weltkrieg/index.html'
+    },
+    {
+        name: 'West und Osteuropa nach 1945',
+        type: 'folder',
+        pathe: 'west-osteuropa-nach-1945/index.html'
+    }
 ];
